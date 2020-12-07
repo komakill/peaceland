b0e9ef0d4273d69136aba11352f190eb83e334e0
JM REMEUR <jm@remeur.fr>
2020-12-07 23:34:53 +0100

    transform string to localdatetime

--- core/src/main/scala/Utils.scala
+++ core/src/main/scala/Utils.scala
@@ -1,5 +1,9 @@
 package core
 
+import java.time.LocalDateTime
+import java.time.Instant
+import java.time.ZoneId
+
 object Utils {
 	val API_URL = "https://peaceland.thomaslacaze.fr"
 	val TELEGRAM_URL = "https://api.telegram.org/bot"
@@ -7,5 +11,10 @@ object Utils {
 
 	def getTelegramUrl(botToken: String): String = {
 		s"$TELEGRAM_URL$botToken$TELEGRAM_URL"
+	}
+
+	def strToDate(str: String): LocalDateTime = {
+		val tmp = Instant.parse(str)
+		LocalDateTime.ofInstant(tmp, ZoneId.systemDefault())
 	}
 }
