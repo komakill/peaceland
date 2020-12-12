9db9e31e5b2f9a36e63bfb4d8ef6e43030ec143e
JM REMEUR <jm@remeur.fr>
2020-12-12 01:14:26 +0100

    fix stats not stopping

--- stats/src/main/scala/Main.scala
+++ stats/src/main/scala/Main.scala
@@ -47,5 +47,8 @@ object Main {
 		println("Question 5:")
 		val percent = result.filter(_.battery > 70).count() * 100 / count
 		println(s"There is $percent% of drones with more than 70% of battery")
+
+		sc.stop()
+		System.exit(0)
 	}
 }
